e34368aa12daed7bb76e17f20256c0c12c371e5a
SanghyunLee <lgw9736@naver.com>
2025-04-13 15:50:48 +0900

    feat: Test 코드 삭제

--- src/test/java/com/practice/ci_cd_practice/CiCdPracticeApplicationTests.java
+++ src/test/java/com/practice/ci_cd_practice/CiCdPracticeApplicationTests.java
@@ -1,8 +1,39 @@
 package com.practice.ci_cd_practice;
 
+import com.practice.ci_cd_practice.entity.User;
+import com.practice.ci_cd_practice.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CiCdPracticeApplicationTests {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//    private User pepper;
+//
+//    @BeforeEach
+//    void setUp() {
+//        pepper = userRepository.save(new User("pepper", "pepper@naver.com"));
+//    }
+//
+//    @Test
+//    void find(){
+//        Optional<User> findUser = userRepository.findById(pepper.getId());
+//
+//        assertThat(findUser).hasValue(pepper);
+//    }
+//
+//    @Test
+//    void delete(){
+//        userRepository.deleteById(pepper.getId());
+//
+//        assertThat(userRepository.findById(pepper.getId())).isEmpty();
+//    }
 }
